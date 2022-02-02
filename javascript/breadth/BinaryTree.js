77b6f996acbf56aec790a75235e50607af858d86
Jacob Choi <jacob.j.choi@gmail.com>
2022-02-01 16:24:41 -0800

    finished

---
+++ javascript/breadth/BinaryTree.js
@@ -0,0 +1,32 @@
+'use strict';
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+    this.newMax = 0;
+  }
+
+  breadthFirst(tree){
+    if(!tree.root){
+      return 'No tree';
+    }
+    console.log('There is a tree!');
+    this.array = [];
+    this.array.push(tree.root);
+    this.output = [];
+    while(this.array.length){
+      const current = this.array.shift();
+      if(current.left){
+        this.array.push(current.left);
+      }
+      if(current.right){
+        this.array.push(current.right);
+      }
+      this.output.push(current.value);
+    }
+    return this.output;
+  }
+}
+
+module.exports = BinaryTree;
+
