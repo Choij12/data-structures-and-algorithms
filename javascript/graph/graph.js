b1bedc23a38bd9d4ae1bc33236749f9d3ed10968
Jacob Choi <jacob.j.choi@gmail.com>
2022-02-25 17:36:47 -0800

    finished challenge

---
+++ javascript/graph/graph.js
@@ -0,0 +1,85 @@
+'use strict';
+
+class Vertex {
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+class Edge {
+  constructor(vertex, weight) {
+    this.vertex = vertex;
+    this.weight = weight;
+  }
+}
+
+class Graph {
+  constructor() {
+    this.adjacencyList = new Map();
+  }
+
+  addVertex(value) {
+    let payload = new Vertex(value);
+    this.adjacencyList.set(payload, []);
+
+    return payload;
+  }
+
+  addDirectedEdge(startVertex, endVertex, weight = 0) {
+    if (
+      !this.adjacencyList.has(startVertex) &&
+        !this.adjacencyList.has(endVertex)
+    ) {
+      throw new Error('Vertex Error');
+    }
+
+    let neighbors = this.adjacencyList.get(startVertex);
+
+    let newEdge = new Edge(endVertex, weight);
+    neighbors.push(newEdge);
+  }
+
+  getNeighbors(vertex) {
+    if (!this.adjacencyList.has(vertex)) {
+      throw new Error('GET NEIGHBOR ERROR :: Invalid vertex');
+    }
+
+    return [...this.adjacencyList.get(vertex)];
+  }
+
+  getNodes() {
+    return this.adjacencyList;
+  }
+
+  size() {
+    return this.adjacencyList.size;
+  }
+}
+
+let graph = new Graph();
+let A = graph.addVertex('A');
+let B = graph.addVertex('B');
+let C = graph.addVertex('C');
+let D = graph.addVertex('D');
+let E = graph.addVertex('E');
+let F = graph.addVertex('F');
+let G = graph.addVertex('G');
+let H = graph.addVertex('H');
+
+graph.addDirectedEdge(A, D);
+graph.addDirectedEdge(A, C);
+graph.addDirectedEdge(A, B);
+graph.addDirectedEdge(D, F);
+graph.addDirectedEdge(B, C);
+graph.addDirectedEdge(B, E);
+graph.addDirectedEdge(C, F);
+graph.addDirectedEdge(C, E);
+graph.addDirectedEdge(C, B);
+graph.addDirectedEdge(F, G);
+graph.addDirectedEdge(G, H);
+graph.addDirectedEdge(H, F);
+
+console.log(graph);
+console.log(JSON.stringify(graph.getNodes()));
+
+module.exports = Graph;
